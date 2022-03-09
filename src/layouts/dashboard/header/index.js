614609e896d24ddd45bc21461b1e6e47083caf5f
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2022-03-09 10:02:05 +0200

    Added AppBar and Footer

--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -1,30 +1,34 @@
 import { alpha, styled } from '@mui/material/styles';
-import { Box, AppBar, Toolbar, Typography } from '@mui/material';
+import { withStyles } from "@material-ui/core/styles";
+import { Box, AppBar, Toolbar, Typography, Container } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 const HEIGHT = 92;
-
-const RootStyle = styled(AppBar)(({ theme }) => ({
-  boxShadow: 'none',
-  backdropFilter: 'blur(6px)',
-  WebkitBackdropFilter: 'blur(6px)',
-  backgroundColor: alpha(theme.palette.background, 0.72),
-  width: `calc(100%}px)`
-}));
+const BG_COLOR = '#f3f6f8';
 
 const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
     minHeight: HEIGHT,
-    padding: theme.spacing(0, 5)
+    padding: theme.spacing(0, 5),
+
+}));
+
+const TextTypography = styled(Typography)(({ theme }) => ({
+    color: theme.palette.text.primary,
+    fontSize: theme.typography.h3.fontSize,
+    fontWeight: theme.typography.h3.fontWeight,
+    lineHeight: theme.typography.h3.lineHeight,
 }));
 
 export default function DashboardNavbar() {
+  const theme = useTheme();
+
   return (
-    <RootStyle>
-      <ToolbarStyle>
-        <Box sx={{ flexGrow: 1 }} />
-          <Typography variant="h6">
-            FilMarket
-          </Typography>
+    <AppBar sx={{ boxShadow: 0, bgcolor: BG_COLOR }}>
+      <ToolbarStyle disableGutters>
+
+          <TextTypography>FilMarket</TextTypography>
+
       </ToolbarStyle>
-    </RootStyle>
+    </AppBar>
   );
 }
