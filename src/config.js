a9bee8dd20291faa9bdd39b0609489eebdca5fbd
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2022-03-30 12:54:49 +0300

    Use production environment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
-export const API = process.env.REACT_APP_API || 'https://rpc.testnet.near.org';
-export const ACCOUNT_ID = process.env.REACT_ACCOUNT_ID || 'filmarket_v7.testnet';
+export const API = process.env.REACT_APP_API || 'https://rpc.mainnet.near.org';
+export const ACCOUNT_ID = process.env.REACT_ACCOUNT_ID || 'filmarket.near';
 export const PADDING = 92;
